d5c25855851f556816480ce76f53fe9ed4c1937e
hiro08gh <hiro08gh@gmail.com>
2020-10-27 19:51:50 +0900

    update: index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-  ],
-}
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  }
+};
