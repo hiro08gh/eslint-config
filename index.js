ec0f77f643ed2a78dff12b156cec6020ed9da20e
hiro08gh <hiro08gh@gmail.com>
2021-04-29 21:11:16 +0900

    add: eslint rule

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:react/recommended', 'standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     ecmaVersion: 2018,
-    "ecmaFeatures": {
-      "jsx": true
+    ecmaFeatures: {
+      jsx: true,
     },
-    sourceType: 'module',
-    project: './tsconfig.json'
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
   },
- plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-  }
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-object-literal-type-assertion": "off",
+    "@typescript-eslint/no-triple-slash-reference": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/prefer-interface": "off",
+    "@typescript-eslint/prefer-namespace-keyword": "off",
+  },
 };
